2bf5a70807b54d270d6b7a1a629f0f84f27c92f4
dmandelb <devin.mandelbaum@gmail.com>
2022-07-07 13:29:35 -0400

    Populate seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+standard_deck = Deck.create(name: "Standard")
+
+standard_deck.suits.create([
+  {name: "Cups"},
+  {name: "Pentacles"},
+  {name: "Swords"},
+  {name: "Wands"}
+])
+standard_deck.suits.each do |suit|
+  suit.cards.create([
+    {value: "Ace"},
+    {value: "Two"},
+    {value: "Three"},
+    {value: "Four"},
+    {value: "Five"},
+    {value: "Six"},
+    {value: "Seven"},
+    {value: "Eight"},
+    {value: "Nine"},
+    {value: "Ten"},
+    {value: "Page"},
+    {value: "Knight"},
+    {value: "Queen"},
+    {value: "King"},
+  ])
+end
+
+
+if Rails.env.development?
+  # Uncomment after users have been Devised
+
+  # user = User.create(username: "devin", email_address: "devin@example.com", password: "password")
+  # ech = user.spreads.create(name: "Energy/Challenge/Advice")
+  # ech.slots.create([
+  #   {name: "Energy"},
+  #   {name: "Challenge"},
+  #   {name: "Advice"}
+  # ])
+end
