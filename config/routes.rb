7b95ec74d2b4445a57d6274c431ae5292f4d3159
dmandelb <devin.mandelbaum@gmail.com>
2022-07-10 20:55:12 -0400

    Create routes and controller infrastructure for slots as nested resource under spreads

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :spreads do
+    resources :slots, except: [:index, :show]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
