cdb3c9ab8eef20cb743815bf96eb006a02ad8db7
dmandelb <devin.mandelbaum@gmail.com>
2022-08-13 14:30:09 -0400

    Add Spread and Slot create workflow

--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -1,7 +1,30 @@
 class SlotsController < ApplicationController
+  before_action :set_spread, only: [:new, :create]
+
+  def new
+    @slot = Slot.new
+  end
+
+  def create
+    @slot = Slot.new(slottable: @spread, name: params[:name])
+    if @slot.save
+      if params[:save_and_finish]
+        redirect_to @spread
+      else
+        redirect_to new_spread_slot_path(@spread)
+      end
+    else
+      render action: 'new'
+    end
+  end
 
   def destroy
     Slot.find(params[:id]).destroy
-    redirect_to Spread.find(params[:spread_id])
+    redirect_to @spread
+  end
+
+  private
+  def set_spread
+    @spread = Spread.find(params[:spread_id])
   end
 end
--- app/controllers/spreads_controller.rb
+++ app/controllers/spreads_controller.rb
@@ -1,4 +1,5 @@
 class SpreadsController < ApplicationController
+  before_action :authenticate_user!
 
   def index
     @spreads = current_user.spreads
@@ -6,6 +7,14 @@ class SpreadsController < ApplicationController
 
   def show
     @spread = Spread.includes(:slots).find(params[:id])
-    # @slots = @spread.slots
+  end
+
+  def new
+    @spread = Spread.new
+  end
+
+  def create
+    @spread = Spread.create(name: params[:spread][:name], user: current_user)
+    redirect_to new_spread_slot_path(@spread)
   end
 end
